5c090b0254a6efbcfec4d61dadbf6fa9f154824c
Rahul K <rahulk27official@gmail.com>
2026-05-18 13:43:38 +0000

    robot not moving

---
+++ build/explore_lite_msgs/explore_lite_msgs__rs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/usernamerahul/agv_ws/build/explore_lite_msgs/explore_lite_msgs__rs
+# Build directory: /home/usernamerahul/agv_ws/build/explore_lite_msgs/explore_lite_msgs__rs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
